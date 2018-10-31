5d21f3b279abd6c88bb67d0e6e70532ba0e9eeb2
Francine Navarro <francine.navarro@shopify.com>
2018-10-31 12:44:13 -0400

    Replace stylesheets with inline CSS to optimize performance

--- lib/shopify_app/engine.rb
+++ lib/shopify_app/engine.rb
@@ -6,13 +6,9 @@ module ShopifyApp
     initializer "shopify_app.assets.precompile" do |app|
       app.config.assets.precompile += %w[
         shopify_app/redirect.js
-        shopify_app/itp_helper.js
-        shopify_app/partition_cookies.js
-        shopify_app/storage_access.js
-        shopify_app/storage_access_redirect.js
-        shopify_app/top_level_interaction.js
-        styles.css
-        itp.css
+        shopify_app/top_level.js
+        shopify_app/enable_cookies.js
+        shopify_app/request_storage_access.js
         storage_access.svg
       ]
     end
